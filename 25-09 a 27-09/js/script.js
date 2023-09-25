23a16da15a5b87ff105fb3c990467d978e888997
LucasLoterio <lucaloterio@gnail.com>
2023-09-25 16:26:19 -0300

    Ordenando os numeros na ordem crescente

---
+++ 25-09 a 27-09/js/script.js
@@ -0,0 +1,47 @@
+let info = [
+];
+
+function inserir(){
+    const numero = parseInt(document.getElementById("numeros").value) 
+        
+     
+       if (!info.includes(numero)) {
+           info.push(numero)
+       }
+        else{
+            alert("insira um numero valido")
+        }
+}
+function organizar(){
+     
+    let index = 0
+    let itens =""
+
+        while(index < info.length) {         
+        itens += `<li>${info[index]} </li>
+         `        
+         
+        index++
+    }
+    document.getElementsByTagName("ul")[0].innerHTML = itens;    
+}
+function bblSort() {
+  
+    for (var i = 0; i < info.length; i++) {
+  
+        
+        for (var j = 0; j < (info.length - i - 1); j++) {
+  
+        
+            if (info[j] > info[j + 1]) {
+  
+             
+                var temp = info[j]
+                info[j] = info[j + 1]
+                info[j + 1] = temp
+            }
+        }
+    }
+    organizar()
+  
+}

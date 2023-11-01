95f11ac938dbce25b10c0ec6161506e03179ef97
Lucas Loterio <lucas.loterio@proway.treina>
2023-11-01 16:55:13 -0300

    Tranformando o exercicio do dia 31-10 em lista

---
+++ 01-11/Carro.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_11
+{
+   
+        internal class Carro
+        {
+            private string placa;
+            private string marca;
+            private string modelo;
+            private string cor;
+
+
+
+
+            public string Placa
+            {
+                get
+                {
+                    return placa;
+                }
+                set
+                {
+                    if (value.Length < 7)
+                    {
+                        Console.WriteLine("Placa invalida");
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        placa = value;
+                    }
+                }
+            }
+            public string Marca
+            {
+                get
+                {
+                    return marca;
+                }
+                set
+                {
+                    if (value.Length < 3)
+                    {
+                        Console.WriteLine("Marca invalida");
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        marca = value;
+                    }
+                }
+            }
+            public string Modelo
+            {
+                get
+                {
+                    return modelo;
+                }
+                set
+                {
+                    if (value.Length < 2)
+                    {
+                        Console.WriteLine("Marca invalida");
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        modelo = value;
+                    }
+                }
+            }
+            public string Cor
+            {
+                get
+                {
+                    return cor;
+                }
+                set
+                {
+                    if (value.Length < 4)
+                    {
+                        Console.WriteLine("Cor invalida");
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        cor = value;
+                    }
+                }
+            }
+
+            public Carro()
+            {
+
+            }
+            public Carro(string placa, string marca, string modelo, string cor)
+            {
+                Placa = placa;
+                Marca = marca;
+                Modelo = modelo;
+                Cor = cor;
+            }
+
+            public override string ToString()
+            {
+                return $"Placa: {this.Placa}\n" +
+                    $"Marca: {this.Marca}\n" +
+                    $"Modelo: {this.Modelo}\n" +
+                    $"Cor: {this.Cor}";
+            }
+
+        }
+    
+}
--- 01-11/Program.cs
+++ 01-11/Program.cs
@@ -1,46 +1,178 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
 
 namespace _01_11
 {
     class Program
     {
+
+        public static List<Carro> carros = new List<Carro>();
         static void Main(string[] args)
         {
-
-            List<Person> persons = new List<Person>();
-            persons.Add(new Person() { Id = 1, Name = "Maria" });
-            persons.Add(new Person() { Id = 2, Name = "Ana" });
-            persons.Add(new Person() { Id = 3, Name = "Pedro" });
-
-            foreach (Person person in persons)
+            bool condicao = false;
+            while (!condicao)
             {
-                Console.WriteLine(person.ToString());
+                Console.WriteLine("Você dejesa utilizar o programa? \n" +
+                    "(sim)   (nao)");
+                
+                string resposta = Console.ReadLine().ToLower();
+                Console.Clear();
+
+                if (resposta == "nao")
+                {
+                    break;
+                }
+                Console.WriteLine($"" +
+                    $"**************************************** \n\n" +
+                    $"   Digite 1 se deseja adicionar \n" +
+                    $"   Digite 2 se deseja alterar \n" +
+                    $"   Digite 3 se deseja excluir \n" +
+                    $"   Digite 4 se dejesa listar \n\n" +
+                    $"****************************************");
+
+                var opcao = Convert.ToInt32(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        adicionar();
+                        break;
+                    case 2:
+                        alterar();
+                        break;
+                    case 3:
+                        excluir();
+                        break;
+                    case 4:
+                        listar();
+                        break;
+                }
             }
+           
 
-            /* List<string> list = new List<string>();
-             list.Add("Ana");
-            Console.WriteLine(list.Count);
-            list.Add("Maria");
-            list.Add("Pedro");
-            Console.WriteLine(list.Count);
 
-             Console.WriteLine("Imprimindo usando forma não indexada");
-             foreach(string item in list)
-             {
-                Console.WriteLine(item);
-             }
-
-             int posSearch = list.IndexOf("Maria");
-             Console.WriteLine(posSearch);
-             list[posSearch] = "Maria das Dores";
-             list.Remove("Pedro");
-
-             Console.WriteLine("Imprimindo usando forma indexada");
-             for(int i = 0; i < list.Count; i++)
-             {
-                 Console.WriteLine(list[i]);
-             }*/
             Console.ReadKey();
         }
+        static void adicionar()
+        {
+            Console.Clear(); try
+            {
+
+
+                Console.WriteLine("Digite a placa do carro");
+                var placa = Console.ReadLine();
+
+                Console.WriteLine("Digite a marca do carro");
+                var marca = Console.ReadLine();
+
+                Console.WriteLine("Digite o modelo do carro");
+                var modelo = Console.ReadLine();
+
+                Console.WriteLine("Digite a cor do carro");
+                var cor = Console.ReadLine();
+
+                for (int index = 0; index < carros.Count; index++)
+                {
+                    if (placa == carros[index].Placa)
+                    {
+                        Console.WriteLine("Placa inalida");
+                        throw new FormatException();
+                    }
+                }
+
+                Carro car = new Carro(placa, marca, modelo, cor);
+
+                carros.Add(car);
+            }
+            catch
+            {
+                
+            }
+
+            
+            Console.Clear();
+
+        }
+
+        static void alterar()
+        {
+            listar();
+            Console.WriteLine("Digite a placa do carro q dejesa alterar");
+            string aux = Console.ReadLine();
+            Console.Clear();
+            try
+            {
+                for (int i = 0; i < carros.Count; i++)
+                {
+                    if (carros[i].Placa.Equals(aux))
+                    {
+                        Console.WriteLine($"" +
+                        $"**************************************** \n\n" +
+                        $"   Digite 1 para alterar a placa\n" +
+                        $"   Digite 2 para alterar a marca\n" +
+                        $"   Digite 3 para alterar a modelo \n" +
+                        $"   Digite 4 para alterar a cor \n\n" +
+                        $"****************************************");
+                    }
+                    int opcao = Convert.ToInt32(Console.ReadLine());
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.WriteLine("Digite nova placa");
+                            string np = Console.ReadLine();
+                            carros[i].Placa = np;
+                            break;
+                        case 2:
+                            Console.WriteLine("Digite nova marca");
+                            string nm = Console.ReadLine();
+                            carros[i].Marca = nm;
+                            break;
+                        case 3:
+                            Console.WriteLine("Digite o novo modelo");
+                            string nmo = Console.ReadLine();
+                            carros[i].Modelo = nmo;
+                            break;
+                        case 4:
+                            Console.WriteLine("Digite nova cor");
+                            string cr = Console.ReadLine();
+                            carros[i].Cor = cr;
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+           
+           
+        }
+
+        static void excluir()
+        {
+            listar();
+            Console.WriteLine("Digite a placa do carro q dejesa excluir");
+            string aux = Console.ReadLine();
+            for (int i = 0; i < carros.Count; i++)
+            {
+                if (carros[i].Placa.Equals(aux))
+                {
+                    carros.RemoveAt(i);
+                }
+            }
+            Console.Clear();
+        }
+        static void listar()
+        {
+            foreach (Carro carro in carros)
+            {
+                Console.WriteLine(carro.ToString());
+            }
+        }
+
+
+
     }
 }

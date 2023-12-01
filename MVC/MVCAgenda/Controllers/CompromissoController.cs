3eb0e4bb3fe43434a1d069b09c9de369dfa15f63
Lucas Loterio <lucas.loterio@proway.treina>
2023-12-01 17:05:22 -0300

    Atualizando o CRUD da agenda feita com MVC, agora adicionando compromissos e locais

---
+++ MVC/MVCAgenda/Controllers/CompromissoController.cs
@@ -0,0 +1,34 @@
+﻿using MVCAgenda.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVCAgenda.Controllers
+{
+    public class CompromissoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Dados.Db.compromissos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            List<SelectListItem> Contatos = new List<SelectListItem>();
+            Contatos = MVCAgenda.Dados.Db.contatos.Select(c => new SelectListItem() 
+            { Text = c.Email, Value = c.Id.ToString() }).ToList();
+            ViewBag.Contatos = Contatos;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Compromisso compromisso)
+        {
+            compromisso.Id = Dados.Db.compromissos.Max(c => c.Id) + 1;
+            Contato ct = Dados.Db.contatos.FirstOrDefault(c => c.Id == compromisso.Contato.Id);
+            compromisso.Contato = ct;
+            Dados.Db.compromissos.Add(compromisso);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/MVCAgenda/Controllers/ContatoController .cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAgenda.Models;
+
+namespace MVCAgenda.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {           
+            return View(Dados.Db.contatos);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Contato contato)
+        {
+            contato.Id = Dados.Db.contatos.Count + 1;
+            Dados.Db.contatos.Add(contato);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            Models.Contato contato = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == id); 
+            return View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Models.Contato contato)
+        {
+            Models.Contato cont = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == contato.Id);
+            cont.Nome = contato.Nome;
+            cont.Email = contato.Email;
+            cont.Fone= contato.Fone;
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            Models.Contato contato = Dados.Db.contatos.FirstOrDefault(ct => ct.Id == id);
+            return View(contato);
+        }
+        public IActionResult Delete(int? id)
+        {
+            Contato contato = Dados.Db.contatos.FirstOrDefault(contato => contato.Id == id);
+
+            return View(contato);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Contato contato = Dados.Db.contatos.FirstOrDefault(contato => contato.Id == id);
+
+            if (contato != null)
+            {
+                Dados.Db.contatos.Remove(contato);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
